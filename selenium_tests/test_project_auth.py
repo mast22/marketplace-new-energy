7114a363091e09719bb2461ffa966748b7d7de8b
Nick <kozhenin.nick@gmail.com>
2019-06-21 02:14:07 +0300

    Added selenium ui tests, debugged JS on role_register

---
+++ selenium_tests/test_project_auth.py
@@ -0,0 +1,83 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import Select
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from users.models import CustomUser
+from django.urls import reverse
+import time
+
+
+class TestProjectAuth(StaticLiveServerTestCase):
+
+    # Конфигурация драйвера
+    opts = Options()
+    opts.headless = True
+
+    def setUp(self):
+        self.browser = Firefox(options=self.opts)
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_register_user_as_contractor(self):
+        self.browser.get(self.live_server_url)
+
+        # тестируем регистрацию и форму регистрации
+
+        email = 'testtest@testtest.ru'
+        password = 'testtestpassword123'
+
+        register_user_url = self.live_server_url + reverse('account_signup')
+        self.browser.get(register_user_url)
+
+        email_input = self.browser.find_element_by_id('id_email')
+        email_input.send_keys(email)
+
+        password_input = self.browser.find_element_by_id('id_password1')
+        password_input.send_keys(password)
+
+        submit_input = self.browser.find_element_by_tag_name('button')
+        submit_input.click()
+
+        # После регистрации необходимо заполнить информацию
+
+        role_register_url = self.live_server_url + \
+            reverse('users:role_register')
+        self.browser.get(role_register_url)
+
+        # Выбираем роль заявителя
+
+        role_input = Select(self.browser.find_element_by_id('id_role'))
+        role_input.select_by_value('contractor')
+
+        # Если роль пользователя подрядчик, то он по умолчанию будет юр. лицом и его нельзя изменить
+
+        person_input = self.browser.find_element_by_id('id_person')
+        self.assertFalse(person_input.is_enabled())
+
+        # person_input = Select(self.browser.find_element_by_id('id_person'))
+        # person_input.select_by_value('entity')
+        #
+        # КОММЕНТАРИЙ К КОДУ ВЫШЕ
+        # FIXME возможный баг. JS автоматически выбирает юр. лицо, однако аттрибут selected не применяется
+        # к значению с юр. лицом. Тем не менее, в БД отправляется выбранное значение
+        # либо на бэкенде стоит автоматическое присвоение, когда выбранно значение подрядчика.
+
+        # заполняем дальше форму
+        first_name_input = self.browser.find_element_by_id('id_first_name')
+        first_name_input.send_keys('Тестовый')
+
+        last_name_input = self.browser.find_element_by_id('id_last_name')
+        last_name_input.send_keys('Пользователь')
+
+        # Если пользователь юр. лицо, то ему доступен ввод названия предприятия.
+        entity_input = self.browser.find_element_by_id('id_entity_name')
+        self.assertTrue(entity_input.is_enabled())
+
+        # Меняем на физ. лицо
+        # entity_input = self.browser.find_element_by_id('id_entity_name')
+
+        # И он должен быть недоступен
+        # self.assertFalse(entity_input.is_enabled())
+
+        # TODO неправильная работа js
--- static/js/role_register.js
+++ static/js/role_register.js
@@ -16,27 +16,65 @@ var remove_option_from_select = function (select, option_value) {
     }
 };
 
-var roleChange = function () {
+var control_entity_features = function() {
+    // Триггер для контроля полей юр. лица
+    permission_upload.disabled = false;
+    staff_upload.disabled = false;
+    equip_upload.disabled = false;
+    exp_upload.disabled = false;
+    reviews_upload.disabled = false;
+    entity_name.disabled = false;
+};
+
+var remove_control_entity_features = function() {
+    person_select.value = '';
+    person_select.disabled = false;
+};
+
+var control_individual_features = function() {
+    // Триггер для контроля полей физ. лица
+    permission_upload.disabled = true;
+    staff_upload.disabled = true;
+    equip_upload.disabled = true;
+    exp_upload.disabled = true;
+    reviews_upload.disabled = true;
+    entity_name.disabled = true;
+    person_select.disabled = false;
+};
+
+var roleChange = function() {
     // Физ. лицо не может быть подрядчиком
     if (role_select.value == 'contractor') {
-        permission_upload.disabled = false;
-        staff_upload.disabled = false;
-        equip_upload.disabled = false;
-        exp_upload.disabled = false;
-        reviews_upload.disabled = false;
-        person_select.disabled = true;
-        entity_name.disabled = false;
+        // Подрядчик может быть только юр. лицом
         person_select.value = 'entity';
+        person_select.disabled = true;
+        control_entity_features();
     }
-    if (role_select.value == 'custome' || !role_select.value){
-        permission_upload.disabled = true;
-        staff_upload.disabled = true;
-        equip_upload.disabled = true;
-        exp_upload.disabled = true;
-        reviews_upload.disabled = true;
-        entity_name.disabled = true;
-        person_select.disabled = false;
+    if (role_select.value == 'custome') {
+        // убираем запреты
+        remove_control_entity_features();
+        // TODO Убрать инпуты для заявителя юр. лица
+    }
+    if (!role_select.value) {
+        // В случае если выбирается "---------", то вырубаем всё
+        control_entity_features();
+        // TODO придумай что нибудь
     }
 };
 
+var personChange = function() {
+    // Юр. лицо всегда должно вводить своё название
+    if (person_select.value == 'individual') {
+        control_individual_features();
+    }
+    if (person_select.value == 'entity') {
+        control_entity_features();
+    }
+    if (!person_select.value) {
+        control_entity_features();
+        // TODO аналог 52 строчки, чёнить надо сделать
+    }
+};
+
+// TODO Удалить из формы на backend
 remove_option_from_select(role_select, 'staff');
